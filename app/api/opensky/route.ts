46042c17bd0cca9857bb1c7e64ac520442a986f1
MaxxximusBR <113954896+MaxxximusBR@users.noreply.github.com>
2025-10-17 19:54:24 -0300

    Update route.ts

--- app/api/opensky/route.ts
+++ app/api/opensky/route.ts
@@ -36,9 +36,13 @@ export async function GET(req: Request) {
         speed: s[9],
         heading: s[10],
         last: s[4],
+        /** 👇 ADICIONE ESTA LINHA */
+        squawk: String(s[14] || ''),   // <- precisamos para detectar 7700
       }));
 
-    return new Response(JSON.stringify({ ok:true, items }), { headers: { 'content-type':'application/json' }});
+    return new Response(JSON.stringify({ ok:true, items }), {
+      headers: { 'content-type':'application/json' }
+    });
   } catch (e:any) {
     return new Response(JSON.stringify({ ok:false, msg: e?.message || 'Falha' }), { status: 500 });
   }
